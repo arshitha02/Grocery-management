9d145c81add8a201146aaf2905a6482648a5dddc
arshitha02 <arshitha.mohan02@gmail.com>
2024-12-06 22:39:04 +0530

    Home page

--- app.py
+++ app.py
@@ -1,1 +1,24 @@
-from flask import Flask
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    # Render the login page with a title
+    return render_template('login.html', title="Login Page")
+
+@app.route('/home')#, methods=['POST'])
+def login():
+    return  render_template('home.html',title="Home page")
+    # Access form data
+    #username = request.form['username']
+    #password = request.form['password']
+
+    # Simple validation (replace with actual authentication)
+   # if username == 'admin' and password == 'admin123':
+    #    return f"Welcome, {username}!"
+    #else:
+     #   return "Invalid credentials, please try again."'''
+
+if __name__ == '__main__':
+    app.run(debug=True)
