758298d89f24d1f149b57e9be1df8aaad37878cb
Athulkrishna-S <athulkrishna768@gmail.com>
2024-12-08 17:40:56 +0530

    Update .gitignore and untrack files

--- app/models/database.py
+++ app/models/database.py
@@ -2,7 +2,12 @@ from pymongo import MongoClient
 
 client = MongoClient('mongodb://localhost:27017/')
 db=client['addon-Grocery']
+
+#collections
 users=db['users']
+items=db['items']
+
 
 def close_db_connection():
     client.close()
+
